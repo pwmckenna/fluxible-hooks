98c544a8f47f068fb53d74d52f40cea55ad1acfc
Patrick Williams <patrickw@porch.com>
2019-03-27 10:23:47 -0700

    babel

--- src/use-stores.js
+++ src/use-stores.js
@@ -1,28 +1,21 @@
-const React = require('react');
-const useContext = require('./use-context');
+import { useEffect, useState } from 'react';
+import useContext from './use-context';
 
-module.exports = function useStores(stores, getStateFromStores) {
+export default (stores, getStateFromStores) => {
   const context = useContext();
-  const s = React.useState(getStateFromStores(context));
-  const state = s[0];
-  const setState = s[1];
-  function onChange() {
-    setState(getStateFromStores(context));
-  }
-  function subscribe() {
-    stores.forEach(function(Store) {
+  const [state, setState] = useState(getStateFromStores(context));
+  const onChange = () => setState(getStateFromStores(context));
+
+  useEffect(() => {
+    stores.forEach((Store) => {
       context.getStore(Store).on('change', onChange);
     });
-  }
-  function unsubscribe() {
-    stores.forEach(function(Store) {
-      context.getStore(Store).removeListener('change', onChange);
-    });
-  }
-  React.useEffect(function () {
-    subscribe();
-    return unsubscribe;
-  });
+    return () => {
+      stores.forEach((Store) => {
+        context.getStore(Store).removeListener('change', onChange);
+      });
+    };
+  }, stores);
 
   return state;
 };
